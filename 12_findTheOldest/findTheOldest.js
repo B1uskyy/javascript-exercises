e5f3fd53e02772fb848f126ab9ebde7a97818ed3
B1uskyy <mats.kvanvik@hotmail.com>
2023-08-20 23:27:38 +0200

    finished exercise 08 - 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+function getAge (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+
+const findTheOldest = function(peopleArray) {
+
+    newArr = [peopleArray[0]]
+
+    for (let i = 1; i < peopleArray.length; i++) {
+        if (getAge(peopleArray[i].yearOfDeath, peopleArray[i].yearOfBirth) > 0)
+        newArr = peopleArray[i]; 
+    }
+    
+    return newArr; 
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
